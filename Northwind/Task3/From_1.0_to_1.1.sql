3e3ee81adaa15b79f5cc9b5bb6d69c418206703f
Dzianis Shyla <Dzianis_Shyla@epam.com>
2017-11-27 02:25:07 +0300

    task 3.2 completed

--- Northwind/Task3/From_1.0_to_1.1.sql
+++ Northwind/Task3/From_1.0_to_1.1.sql
@@ -1,7 +1,7 @@
 ﻿IF NOT EXISTS (select * from sys.tables t where t.name = 'CreditCards')
 BEGIN
-    CREATE TABLE CreditCards(
-        CreditCardId INT PRIMARY KEY IDENTITY(1,1),
+    CREATE TABLE [dbo].[CreditCards](
+        CreditCardId INT NOT NULL PRIMARY KEY IDENTITY(1,1),
         ExpirationDate DATETIME DEFAULT(NULL),
         CardHolderName VARCHAR(200) NOT NULL,
         EmployeeId INT REFERENCES Employees(EmployeeID)
--- Northwind/Task3/From_1.1_to_1.3.sql
+++ Northwind/Task3/From_1.1_to_1.3.sql
@@ -2,3 +2,9 @@
 BEGIN
     EXEC sp_rename 'Region', 'Regions';
 END
+
+IF NOT EXISTS (select * from sys.columns where object_id = OBJECT_ID(N'[dbo].[Customers]') AND name = 'FoundationDate')
+BEGIN
+    ALTER TABLE Customers
+    ADD FoundationDate DATETIME
+END
