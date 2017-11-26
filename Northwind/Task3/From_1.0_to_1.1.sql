ff73c3f5ff41dabc00e0b1e3a75621f9dfce2aea
Dzianis Shyla <Dzianis_Shyla@epam.com>
2017-11-27 01:55:46 +0300

    task 3.1 completed

---
+++ Northwind/Task3/From_1.0_to_1.1.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS (select * from sys.tables t where t.name = 'CreditCards')
+BEGIN
+    CREATE TABLE CreditCards(
+        CreditCardId INT PRIMARY KEY IDENTITY(1,1),
+        ExpirationDate DATETIME DEFAULT(NULL),
+        CardHolderName VARCHAR(200) NOT NULL,
+        EmployeeId INT REFERENCES Employees(EmployeeID)
+        );
+END
---
+++ Northwind/Task3/From_1.1_to_1.3.sql
@@ -0,0 +1,4 @@
+﻿IF EXISTS (select * from sys.tables t where t.name = 'Region')
+BEGIN
+    EXEC sp_rename 'Region', 'Regions';
+END
