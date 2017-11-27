6d32085fde2da057c61df27e23438a11d564092e
Dzianis_Shyla <Dzianis_Shyla@epam.com>
2017-11-27 09:24:12 +0300

    task 2.2.5 finished

--- Northwind/Task2/Task2.2/5_subtask.sql
+++ Northwind/Task2/Task2.2/5_subtask.sql
@@ -1,2 +1,7 @@
 ﻿--Найти всех покупателей, которые живут в одном городе
-select * from Customers where City='Berlin';
+select c1.CustomerID as 'Customer id'
+       ,c2.CustomerID as 'Neighbor id'
+       ,c1.City as 'City'
+from Customers c1
+    left join Customers c2 ON c1.CustomerID <> c2.CustomerID AND c1.City = c2.City
+order by c1.CustomerID;
